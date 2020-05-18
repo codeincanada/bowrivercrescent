7e7fe6eb17b63f74e46cdf0390d821d746dc487b
Daniel Santos <daniel@codein.ca>
2020-05-18 18:09:31 -0400

    add basic styling

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to the Bow River Crescent community!</h1>
+            </header>
+            <main className="App-main">
+                <input type="button" value="Sign in with Google"/>
+                <a href="#">Sign up</a>
+            </main>
+        </div>
+    );
 }
 
 export default App;
